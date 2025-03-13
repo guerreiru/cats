28e20e7d26bc18475c00a9b9e9c7c7ef183ee179
Fernando Guerreiro <f.luhis@gmail.com>
2025-03-13 19:51:32 -0300

    Atualiza a URL base para buscar gato e conselho utilizando NEXT_PUBLIC_BASE_URL

--- src/lib/getCatAndAdvice.ts
+++ src/lib/getCatAndAdvice.ts
@@ -1,11 +1,10 @@
 export async function getCatAndAdvice() {
-  const baseUrl = process.env.VERCEL_URL
-    ? `https://${process.env.VERCEL_URL}`
-    : "http://localhost:3000";
-
-  const res = await fetch(`${baseUrl}/api/catAndAdvice`, {
-    cache: "no-store",
-  });
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_BASE_URL}/api/catAndAdvice`,
+    {
+      cache: "no-store",
+    }
+  );
 
   if (!res.ok) {
     throw new Error("Erro ao buscar dados do gato e conselho.");
