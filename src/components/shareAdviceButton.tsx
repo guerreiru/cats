8283f4f521a34ebee17d0aefcae10b1388a1342f
Fernando Guerreiro <f.luhis@gmail.com>
2025-03-14 11:10:59 -0300

    Remove a URL do tweet no botão de compartilhamento de conselhos

--- src/components/shareAdviceButton.tsx
+++ src/components/shareAdviceButton.tsx
@@ -13,7 +13,7 @@ export function ShareAdviceButton({ advice, image }: ShareAdviceButton) {
     const imageUrl = encodeURIComponent(image);
     const tweetUrl = `https://twitter.com/intent/tweet?text=${encodeURIComponent(
       text
-    )}&url=${imageUrl}`;
+    )}`;
     window.open(tweetUrl, "_blank");
   };
   return (
@@ -21,7 +21,7 @@ export function ShareAdviceButton({ advice, image }: ShareAdviceButton) {
       onClick={shareAdvice}
       className="cursor-pointer w-full flex items-center justify-center gap-1.5 px-6 py-3 bg-gray-800 hover:bg-gray-900 text-white text-lg font-semibold rounded-full transition-all duration-300 shadow-md hover:scale-105"
     >
-      <FaXTwitter /> Compartilhar
+      <FaXTwitter /> Twittar
     </button>
   );
 }
